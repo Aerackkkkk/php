118cd14cde28d336c226bf0d3ac4c3f80a63eb34
Anjal Thapa <71475450+Aerackkkkk@users.noreply.github.com>
2022-06-17 15:38:43 +0545

    Add files via upload

---
+++ h1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP test </title>
+    <h1><p style="text align:center; color:Velvet; font-size:30px;">Lets Learn Php</h1>
+</head>
+<body>
+    <?php
+    echo"Can't i learn php and more";
+    ?>
+</body>
+</html>h1.php
